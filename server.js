4b2a8d38b487ec46d454e03ecc75b854a49b99b7
Walter Cameron <walter@waltercameron.com>
2016-03-13 13:39:41 -0800

    gzip all the things

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express')
 var serveStatic = require('serve-static')
+var compress = require('compression')
 
 var app = express()
 
+app.use(compress())
 app.use(serveStatic('public', {'index': ['index.html', 'index.htm']}))
 app.listen(process.env.PORT || 8400)
